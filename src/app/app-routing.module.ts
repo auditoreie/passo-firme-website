302903014a8b2e42af724235a2bfad50f540519c
xArthurDev <arthurrguimaraes12@gmail.com>
2021-04-08 23:40:17 -0300

    Add dashboard page to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -31,6 +31,9 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./pages/dashboard/dashboard.module').then( m => m.DashboardPageModule)
   }
 ];
 
